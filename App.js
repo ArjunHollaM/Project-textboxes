fde7e5b31e682a221fa6b358a27ce4a1cbab2c18
ArjunHollaM <67258488+ArjunHollaM@users.noreply.github.com>
2021-09-13 16:11:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -7,12 +7,13 @@ class App extends Component {
   render() { 
     return (
       <div className = 'Editors'>
-        <Editor id = 'html' placeholder = 'Write HTML code here' />
-        <Editor id = 'css' placeholder = 'Write CSS code here' />
-        <Editor id = 'js' placeholder = 'Write JavaScript code here' />
+        <Editor id = 'html' placeholder = 'Write HTML code here' editorTitle = "HTML"/>
+        <Editor id = 'css' placeholder = 'Write CSS code here' editorTitle = "CSS"/>
+        <Editor id = 'js' placeholder = 'Write JavaScript code here' editorTitle = "Java Script"/>
       </div>
       );
   }
 }
 
 export default App;
+
