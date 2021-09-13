f115ee2d7c706cbe9e39b126a8e45f67d808935f
ArjunHollaM <67258488+ArjunHollaM@users.noreply.github.com>
2021-09-13 16:12:20 +0530

    Update Editor.js

--- Editor.js
+++ Editor.js
@@ -1,12 +1,16 @@
-import React  from "react";
+import React, {useState} from "react";
 
 
 
 const Editor = (props) =>  {
-    
+    const [isOpen,setOpen] = useState(true);
+    const handleToggle = () => {
+        setOpen(!isOpen);
+    }
     return (
-        <div>
-            <textarea className = "AllEditors" row = "7" id = {props.id} placeholder = {props.placeholder}></textarea>
+        <div className = 'AllEditors'>
+            <div className = "titles">{props.editorTitle}<button onClick = {handleToggle}>{isOpen? "[x]":"[+}"}</button></div>
+            <textarea className = {isOpen ? "opened" : "closed"} row = "7" id = {props.id} placeholder = {props.placeholder}></textarea>
         </div>
     );
 }
